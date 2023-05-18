1b856b3217448ba860c6d1a0c58282b7b0fd88c2
TiferKing <TiferKing@126.com>
2023-05-19 00:36:18 +0800

    Optimize filter design with caching

--- tool/DemixingResampleFilter.m
+++ tool/DemixingResampleFilter.m
@@ -22,7 +22,20 @@ function [Filter] = DemixingResampleFilter(RFSampleRate, BaseSampleRate)
 %   Please refer to the 'LICENSE' file included in the root directory 
 %   of the project.
 
-    [N, Fo, Ao, W] = firpmord([BaseSampleRate / 2, BaseSampleRate] / (RFSampleRate / 2), [1 0], [0.05, 1e-05]);
-    Filter = firpm(N, Fo, Ao, W, {20});
+    persistent RFSa;
+    persistent BaseSa;
+    persistent DesignedFilter;
+    if (isempty(RFSa) || isempty(BaseSa) || isempty(DesignedFilter))
+        RFSa = RFSampleRate;
+        BaseSa = BaseSampleRate;
+        [N, Fo, Ao, W] = firpmord([BaseSampleRate / 2, BaseSampleRate] / (RFSampleRate / 2), [1 0], [0.05, 1e-05]);
+        DesignedFilter = firpm(N, Fo, Ao, W, {20});
+    elseif ((RFSa ~= RFSampleRate) || (BaseSa ~= BaseSampleRate))
+        RFSa = RFSampleRate;
+        BaseSa = BaseSampleRate;
+        [N, Fo, Ao, W] = firpmord([BaseSampleRate / 2, BaseSampleRate] / (RFSampleRate / 2), [1 0], [0.05, 1e-05]);
+        DesignedFilter = firpm(N, Fo, Ao, W, {20});
+    end
+    Filter = DesignedFilter;
 end
 
