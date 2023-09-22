e2830989dd302ce717e57c7af19cd074f53a76ca
TiferKing <TiferKing@126.com>
2023-09-23 01:35:50 +0800

    Optimize filter generator
    Add a limit to filter generator to prevent the poor performance when the BaseSampleRate is far less than RFSampleRate

--- tool/DemixingResampleFilter.m
+++ tool/DemixingResampleFilter.m
@@ -25,15 +25,19 @@ function [Filter] = DemixingResampleFilter(RFSampleRate, BaseSampleRate)
     persistent RFSa;
     persistent BaseSa;
     persistent DesignedFilter;
-    if (isempty(RFSa) || isempty(BaseSa) || isempty(DesignedFilter))
+    if (isempty(RFSa) || isempty(BaseSa) || isempty(DesignedFilter) || (RFSa ~= RFSampleRate) || (BaseSa ~= BaseSampleRate))
         RFSa = RFSampleRate;
         BaseSa = BaseSampleRate;
-        [N, Fo, Ao, W] = firpmord([BaseSampleRate / 2, BaseSampleRate] / (RFSampleRate / 2), [1 0], [0.05, 1e-05]);
-        DesignedFilter = firpm(N, Fo, Ao, W, {20});
-    elseif ((RFSa ~= RFSampleRate) || (BaseSa ~= BaseSampleRate))
-        RFSa = RFSampleRate;
-        BaseSa = BaseSampleRate;
-        [N, Fo, Ao, W] = firpmord([BaseSampleRate / 2, BaseSampleRate] / (RFSampleRate / 2), [1 0], [0.05, 1e-05]);
+        FilterFactor = BaseSampleRate / RFSampleRate;
+        if(FilterFactor < 0.005)
+            % When the baseband sample rate is significantly below the RF 
+            % sample rate, designing a high-performance filter becomes 
+            % challenging. Therefore, it is advisable to establish a limit 
+            % for the filter factor to mitigate potential degradation in 
+            % performance under these circumstances.
+            FilterFactor = 0.005;
+        end
+        [N, Fo, Ao, W] = firpmord([FilterFactor, FilterFactor * 2], [1 0], [0.05, 1e-05]);
         DesignedFilter = firpm(N, Fo, Ao, W, {20});
     end
     Filter = DesignedFilter;
